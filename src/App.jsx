d0ec076c0f258719d84321f3889f813d4ca81d0d
ACarolinaGonzalezA <acarolina.gonzaleza@gmail.com>
2024-08-13 16:49:33 -0500

    agregando clase6

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,7 @@ function App() {
       <TaskButton task="Clase DH" onAddTask={addTask} />
       <TaskButton task="Lectura y Desacanso" onAddTask={addTask} />
       <TaskList tasks={tasks} />
+      
     </>
   )
 }
